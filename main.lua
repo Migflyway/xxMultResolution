823632a8418a8a22b780d43967ac1cbb67d94ebd
Migflyway <wenchenggui@gmail.com>
2018-11-18 14:38:12 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 init("0", 1); --以当前应用 Home 键在右边初始化
 
 
---height is large   width is small for a phone
+--height is large, width is small for a phone
+-- 首先要明白缩放比，再进行处理
+-- 对于一个 控件，有一个锚点【及原点】，算出在开发分辨率下与原点的位置。再按照缩放比进行缩放，然后即可带入到新的锚点中去 求出新坐标
 
 --16 / 9
 -- 0.5625
@@ -32,9 +34,12 @@ dep_nine_pos = {
 --width,height = getScreenSize()
 -- 0.75
 width,height = 768, 1024
+
+--发现这种黑边处理有点问题
+--[[
+
 local dif_width, dif_height = 0, 0
 
---[[
 -- 纵向检测
 -- 从高的中间开始检测
 for i=1, width,1 do
@@ -58,8 +63,6 @@ for i=1, height,1 do
 	end
 end
 ]]
-
-dif_width = 95
 --- 4 / 3 (4 * 4 = 16 / 12)
 --width,height = 768, 1024
 cur_x = math.max(width,height)
@@ -67,17 +70,17 @@ cur_y = math.min(width,height)
 
 print("运行环境 x轴"..cur_x.." y轴"..cur_y)
 nine_pos = {
-	{0,0+dif_width},
-	{cur_x/2,0+dif_width},
-	{cur_x,0+dif_width},
+	{0,0},
+	{cur_x/2,0},
+	{cur_x,0},
 	
 	{0,cur_y/2},
 	{cur_x/2,cur_y/2},
 	{cur_x,cur_y/2},
 	
-	{0,cur_y-dif_width},
-	{cur_x/2,cur_y-dif_width},
-	{cur_x,cur_y-dif_width}
+	{0,cur_y},
+	{cur_x/2,cur_y},
+	{cur_x,cur_y}
 }
 
 
@@ -86,26 +89,13 @@ if (dep_width/dep_height) < (width / height) then
 	sacale_rate = dep_x/cur_x
 elseif (dep_width/dep_height) > (width / height) then
 	sacale_rate = dep_y/cur_y
+else
+	sacale_rate = 1
 end
+
 print("当前缩放比例"..sacale_rate)
 
 function returnpos(arcpos,array)
-	--[[
-	if arcpos==1 then
-		--return ((array[1]-dep_nine_pos[arcpos][1])/dep_x)*cur_x, ((array[2]-dep_nine_pos[arcpos][2])/dep_y)*cur_y
-		return (array[1]-dep_nine_pos[arcpos][1]) / (dep_x/cur_x) + nine_pos[arcpos][1], (array[2]-dep_nine_pos[arcpos][2])/ (dep_x/cur_x)  + nine_pos[arcpos][2]
-	elseif arcpos==2 then
-		return ((array[1]-dep_nine_pos[arcpos][1])/dep_x)*cur_x, ((array[2]-dep_nine_pos[arcpos][2])/dep_y)*cur_y
-	elseif arcpos==3 then
-		return (array[1]-dep_nine_pos[arcpos][1]) / (dep_x/cur_x) + nine_pos[arcpos][1], (array[2]-dep_nine_pos[arcpos][2])/ (dep_x/cur_x)  + nine_pos[arcpos][2]
-		--return (array[1]/dep_nine_pos[arcpos][1]) * nine_pos[arcpos][1], (array[2]-dep_nine_pos[arcpos][2]) * (dep_x/cur_x) + nine_pos[arcpos][2]
-	elseif arcpos==4 then
-		return (array[1]-dep_nine_pos[arcpos][1]) / (dep_x/cur_x) + nine_pos[arcpos][1], (array[2]-dep_nine_pos[arcpos][2])/ (dep_x/cur_x)  + nine_pos[arcpos][2]
-	elseif arcpos==5 or arcpos==6 or arcpos==7 or arcpos==8 or arcpos==9 then
-		return (array[1]-dep_nine_pos[arcpos][1]) / (dep_x/cur_x) + nine_pos[arcpos][1], (array[2]-dep_nine_pos[arcpos][2])/ (dep_x/cur_x)  + nine_pos[arcpos][2]
-	end
-	]]
-	
 	return math.floor((array[1]-dep_nine_pos[arcpos][1]) / sacale_rate + nine_pos[arcpos][1]), math.floor((array[2]-dep_nine_pos[arcpos][2])/ sacale_rate  + nine_pos[arcpos][2])
 end
 
